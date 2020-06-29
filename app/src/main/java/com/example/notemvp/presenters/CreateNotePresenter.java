8db973798bd5b267970e6d12f517e5edbe15ecf3
AndrewBlack-hub <crys2014@mail.ru>
2020-06-29 21:23:34 +0300

    create second screen presenter and interface second screen presenter

---
+++ app/src/main/java/com/example/notemvp/presenters/CreateNotePresenter.java
@@ -0,0 +1,19 @@
+package com.example.notemvp.presenters;
+
+public class CreateNotePresenter implements ICreateNotePresenter {
+    @Override
+    public void saveNote() {
+    }
+
+    @Override
+    public void onClickedBtnHome() {
+    }
+
+    @Override
+    public void editValidation() {
+    }
+
+    @Override
+    public void date() {
+    }
+}
---
+++ app/src/main/java/com/example/notemvp/presenters/ICreateNotePresenter.java
@@ -0,0 +1,8 @@
+package com.example.notemvp.presenters;
+
+public interface ICreateNotePresenter {
+    void saveNote();
+    void onClickedBtnHome();
+    void editValidation();
+    void date();
+}
