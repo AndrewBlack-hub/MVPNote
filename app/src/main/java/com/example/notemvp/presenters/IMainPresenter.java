eabdfe6d1d7db616915f39ccde66abf9d83ff9d6
AndrewBlack-hub <crys2014@mail.ru>
2020-06-29 16:05:56 +0300

    create main presenter and interface main presenter

---
+++ app/src/main/java/com/example/notemvp/presenters/IMainPresenter.java
@@ -0,0 +1,7 @@
+package com.example.notemvp.presenters;
+
+public interface IMainPresenter {
+    void onNoteClicked();
+    void deleteNote(int position);
+    void onCreateNoteClicked();
+}
---
+++ app/src/main/java/com/example/notemvp/presenters/MainFragmentPresenter.java
@@ -0,0 +1,15 @@
+package com.example.notemvp.presenters;
+
+public class MainFragmentPresenter implements IMainPresenter {
+    @Override
+    public void onNoteClicked() {
+    }
+
+    @Override
+    public void deleteNote(int position) {
+    }
+
+    @Override
+    public void onCreateNoteClicked() {
+    }
+}
