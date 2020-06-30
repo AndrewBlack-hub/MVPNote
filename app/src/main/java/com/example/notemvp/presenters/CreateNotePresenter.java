14f0309cfbb749a086d2ff9e27033f654e07657b
AndrewBlack-hub <crys2014@mail.ru>
2020-06-30 13:15:23 +0300

    rewrite titles methods 2

--- app/src/main/java/com/example/notemvp/presenters/CreateNotePresenter.java
+++ app/src/main/java/com/example/notemvp/presenters/CreateNotePresenter.java
@@ -1,16 +1,17 @@
 package com.example.notemvp.presenters;
 
 public class CreateNotePresenter implements ICreateNotePresenter {
+
+    private void validation() {
+        
+    }
+
     @Override
     public void saveNote() {
     }
 
     @Override
     public void onClickedBtnHome() {
-    }
-
-    @Override
-    public void editValidation() {
     }
 
     @Override
--- app/src/main/java/com/example/notemvp/presenters/ICreateNotePresenter.java
+++ app/src/main/java/com/example/notemvp/presenters/ICreateNotePresenter.java
@@ -3,6 +3,5 @@ package com.example.notemvp.presenters;
 public interface ICreateNotePresenter {
     void saveNote();
     void onClickedBtnHome();
-    void editValidation();
     void date();
 }
